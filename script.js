67a363e6e71ac8e548fc6eb8a8120ae6c56bf270
rijjashar405 <rijjashar405@gmail.com>
2026-04-16 20:46:45 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,93 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlides(n) {
+    if (n >= slides.length) {
+        slideIndex = 0;
+    } else if (n < 0) {
+        slideIndex = slides.length - 1;
+    } else {
+        slideIndex = n;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[slideIndex].classList.add("active");
+}
+
+function changeSlide(n) {
+    showSlides(slideIndex + n);
+}
+
+setInterval(() => {
+    changeSlide(1);
+}, 1000);
+
+let currentMenuIndex = 0;
+const menuPages = document.querySelectorAll(".menu-page");
+
+function showMenuPage(n) {
+    if (menuPages.length === 0) return;
+
+    if (n >= menuPages.length) {
+        currentMenuIndex = 0;
+    }
+    else if (n < 0) {
+        currentMenuIndex = menuPages.length - 1;
+    }
+    else {
+        currentMenuIndex = n;
+    }
+    menuPages.forEach(page => {
+        page.classList.remove("active");
+    })
+    menuPages[currentMenuIndex].classList.add("active");
+}
+
+function changeMenuPage(n) {
+    showMenuPage(currentMenuIndex + n);
+}
+// CONTACT SLIDER
+
+let contactIndex = 0;
+let contactSlides = [];
+
+// RUN AFTER PAGE LOAD
+document.addEventListener("DOMContentLoaded", () => {
+
+    contactSlides = document.querySelectorAll(".contact-slide");
+
+    showContactSlide(0);
+
+    // AUTO SLIDE
+    setInterval(() => {
+        changeContactSlide(1);
+    }, 3000);
+});
+
+// SHOW SLIDE
+function showContactSlide(index) {
+
+    if (contactSlides.length === 0) return;
+
+    if (index >= contactSlides.length) {
+        contactIndex = 0;
+    } else if (index < 0) {
+        contactIndex = contactSlides.length - 1;
+    } else {
+        contactIndex = index;
+    }
+
+    contactSlides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    contactSlides[contactIndex].classList.add("active");
+}
+
+// BUTTONS
+function changeContactSlide(n) {
+    showContactSlide(contactIndex + n);
+}
